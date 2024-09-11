796a564fdb7d64677c060414a2a6b309d7ef605d
sriharini63 <sriharinimk@mitrahsoft.com>
2024-09-11 19:09:02 +0530

    c# task1

---
+++ C#/Static and Non-static Task/Static and Non-static Task/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Static_and_Non_static_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("static within static of same class");
+            StaticMethod();
+
+
+            Console.WriteLine("Non static within static of same class");
+            Program obj = new Program();
+            obj.NonStaticMethod();
+
+
+            Console.WriteLine("Non static within static of other class");
+            StaticClass myObj = new StaticClass();
+            myObj.NonstaticFromOtherClass();
+        }
+        static void StaticMethod()
+        {
+            Console.WriteLine("method one");
+
+            Console.WriteLine("static within static of other class");
+              StaticClass.Method();
+        }
+        void NonStaticMethod()
+        {
+            Console.WriteLine("method two");
+
+
+            Console.WriteLine("Non static within non static of same class");
+            NonStaticMethods();
+
+
+            Console.WriteLine("static within non static of same class");
+            StaticMethod();
+        }
+         void NonStaticMethods()
+        {
+            Console.WriteLine("method three");
+
+
+            Console.WriteLine("Non static within non static of other class");
+            StaticClass myObject = new StaticClass();
+            myObject.NonstaticFromOtherClass();
+
+
+            Console.WriteLine("static within non static of other class");
+            StaticClass.Method();
+        }
+    }
+
+}
---
+++ C#/Static and Non-static Task/Static and Non-static Task/StaticClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_and_Non_static_Task
+{
+    class StaticClass
+    {
+        public void NonstaticFromOtherClass()
+        {
+            Console.WriteLine("methodFour");
+        }
+
+        public static void Method()
+        {
+            Console.WriteLine("methodFive");
+        }
+    }
+}
