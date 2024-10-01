d3633211c5608acc80e22e8880e2c5599abdeea0
giritharan-d <giritharan.d@mitrahsoft.com>
2024-10-01 19:46:38 +0530

    CRUD Web API

---
+++ CRUD_Web_API/Entity/Users.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_Web_API.Entity
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string  UserName { get; set; }
+   
+        [Required]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+       
+    }
+}
